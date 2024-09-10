128abd29c7a1271dc4df352620854161c80f1bd5
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-09-10 17:37:10 +0530

    modified index.html added objects.js

---
+++ objects.js
@@ -0,0 +1,11 @@
+var tutorial = {
+  topic: "Javascript",
+  trainer: "sp",
+  mode: "online",
+};
+
+document.write("Topic: " + tutorial.topic);
+document.write("<br/> <br/>");
+document.write("Trainer: " + tutorial.trainer);
+document.write("<br/> <br/>");
+document.write("Mode: " + tutorial.mode);
