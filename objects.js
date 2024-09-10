71954138b9c0a32b49cd1cb997c7c9d81a814c05
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-09-10 19:16:01 +0530

    modified objects.js in .

--- objects.js
+++ objects.js
@@ -9,3 +9,10 @@ document.write("<br/> <br/>");
 document.write("Trainer: " + tutorial.trainer);
 document.write("<br/> <br/>");
 document.write("Mode: " + tutorial.mode);
+document.write("<br/> <br/>");
+
+//from a for loop
+for (var key in tutorial) {
+  document.write(key + ":" + tutorial[key]);
+  document.write("<br/>");
+}
